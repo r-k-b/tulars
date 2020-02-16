3a4e0547bb5c45c50690ce746ecfadb729350394
Robert K. Bell <robert.k.bell@gmail.com>
2020-02-16 21:31:58 +1100

    chore: remove unused imports/modules

--- app/Util.elm
+++
@@ -1,8 +0,0 @@
-module Util exposing (mousePosToVec2)
-
-import Vector2d as V2 exposing (Vector2d)
-
-
-mousePosToVec2 : { x : Int, y : Int } -> Vector2d
-mousePosToVec2 p =
-    V2.fromComponents ( toFloat p.x, toFloat p.y )
