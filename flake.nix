e8330be0a4eb2861ce1706bb52fa77f457c9dc03
Robert K. Bell <robert.k.bell@gmail.com>
2023-11-27 13:54:46 +1100

    build: let dependency updates work properly

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   description = "Trying out flakes with Tulars";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     let supportedSystems = with flake-utils.lib.system; [ x86_64-linux ];
