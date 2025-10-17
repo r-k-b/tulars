0b6f7d66aaa4e95d67fc68a24901c12fa1b0f6fd
Robert K. Bell <robert.k.bell@gmail.com>
2025-10-18 09:23:49 +1100

    refactor(build): stop referencing the wrong version

--- nix/elmReviewed.nix
+++ nix/elmReviewed.nix
@@ -1,5 +1,9 @@
 { elm-review-tool, elmPackages, elmVersion, lib, pkgs, stdenv, reviewSrc }:
-let mainApp = builtins.fromJSON (builtins.readFile ../elm.json);
+let
+  mainApp = builtins.fromJSON (builtins.readFile ../elm.json);
+
+  reviewApp = builtins.fromJSON (builtins.readFile ../review/elm.json);
+  elmReviewVersion = reviewApp.dependencies.direct."jfmengels/elm-review";
 
 in stdenv.mkDerivation {
   name = "elm-reviewed";
@@ -13,8 +17,8 @@ in stdenv.mkDerivation {
       extraDeps = mainApp.dependencies.direct // mainApp.dependencies.indirect;
     }}
     set -e
-    mkdir -p .elm/elm-review/2.12.0
-    ln -s ../../${elmVersion} .elm/elm-review/2.12.0/${elmVersion}
+    mkdir -p .elm/elm-review/${elmReviewVersion}
+    ln -s ../../${elmVersion} .elm/elm-review/${elmReviewVersion}/${elmVersion}
     elm-review --offline
     echo "passed" > $out
   '';
