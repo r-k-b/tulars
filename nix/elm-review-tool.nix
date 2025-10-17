8619f9ab2c9b7cc5b7dcb7de3e5dd2aecb64f089
Robert K. Bell <robert.k.bell@gmail.com>
2025-10-18 09:42:06 +1100

    chore: update elm deps

--- nix/elm-review-tool.nix
+++ nix/elm-review-tool.nix
@@ -10,6 +10,12 @@ pkgs.buildNpmPackage {
   buildPhase = ''
     substituteInPlace ./package.json \
       --replace-fail '"elm-tooling install"' '"echo skipping elm-tooling"'
+
+    # Leaving this at 1.1.3 causes build failures, with:
+    # `Error: Because there is no version of elm/json in 1.1.3 and root/ 0.0.0 depends on elm/json 1.1.3, root/ 0.0.0 is forbidden.`
+    substituteInPlace ./parseElm/elm.json \
+      --replace-fail '"elm/json": "1.1.3"' '"elm/json": "1.1.4"'
+
     mkdir -p "$out"
     cp -r * "$out"/
     mv $out/bin/elm-review $out/bin/elm-review.js
